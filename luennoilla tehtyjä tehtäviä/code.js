e48137f798218c79686a2915faa8f91396649988
heliannika <heliannikar@outlook.com>
2022-05-11 09:41:07 +0300

    DomPerusteet

---
+++ luennoilla tehtyjä tehtäviä/code.js
@@ -0,0 +1,35 @@
+// a
+elem = document.querySelector("#content");
+console.log(elem.tagName);
+
+// b
+elem = document.querySelector(".basic");
+console.log(elem.tagName);
+
+// c
+
+let liElems = document.querySelectorAll("li");
+liElems = document.getElementsByTagName("li");
+
+for(elem of liElems) {
+    console.log(elem.textContent);
+}
+
+// d
+let bodyDesc = document.querySelectorAll("body *");
+for(elem of bodyDesc) {
+    console.log(elem.nodeName);
+}
+
+// e
+
+let articleDesc = document.querySelectorAll("article p");
+for (elem of articleDesc) {
+    console.log(elem.textContent);
+}
+
+// f
+
+for(elem of liElems) {
+    elem.textContent = "John Doe";
+}
